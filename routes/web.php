ef02622f0bf92f870d5bb9772663148c8641e233
Felipe <ninofelipe73@gmail.com>
2020-04-05 17:17:04 -0500

    setting up welcome blade

--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/login', function () {
-    return view('welcome');
+Route::get('/', function () {
+    return view('/auth/login');
 });
 
 
