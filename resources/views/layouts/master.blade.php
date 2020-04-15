ef4d9a5870bc8a8fdffd7cd101464921152ced1c
Nathan <englernathan@gmail.com>
2020-04-15 17:14:10 -0500

    finished merging

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -11,14 +11,15 @@
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
   <style type="text/css">
-<<<<<<< HEAD
     .bs-example {
-      margin: 20px;}
-      a.active {  
-  background-color: #426ff5;
-  color: white;
-}
-=======
+      margin: 20px;
+    }
+
+    a.active {
+      background-color: #426ff5;
+      color: white;
+    }
+
     html,
     body {
       width: 100%;
@@ -99,7 +100,6 @@
     .navbar-togger-icon {
       background-image: url("data:image/svg+xml;charset=utf8,%3Csvg viewBox='0 0 32 32' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath stroke='rgba(255,102,203, 0.5)' stroke-width='2' stroke-linecap='round' stroke-miterlimit='10' d='M4 8h24M4 16h24M4 24h24'/%3E%3C/svg%3E");
     }
->>>>>>> 457618502ff46ba920c449d4e78898a4e5ff1c64
   </style>
 
 </head>
@@ -114,53 +114,46 @@
     <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
       <span class="navbar-toggler-icon"></span>
     </button>
-<<<<<<< HEAD
     <div class="collapse navbar-collapse" id="navbarCollapse">
       <div class="navbar-nav">
-      
-        <a href="/home" class="nav-item nav-link {{ active('home') }}">Home</a> 
-        <a href="/reports" class="nav-link {{ active('reports') }}">Reports</a></li> 
-        <a href="/overview" class="nav-item nav-link {{ active('overview') }}">Overview</a>
-      
-      </div>
-=======
 
-    @auth
-    <div class="collapse navbar-collapse" id="navbarCollapse">
-
->>>>>>> 457618502ff46ba920c449d4e78898a4e5ff1c64
-      <div class="navbar-nav ml-auto">
         <a href="/home" class="nav-item nav-link {{ active('home') }}">Home</a>
-        <a href="/report" class="nav-item nav-link {{ active('report') }}">Reports</a>
+        <a href="/reports" class="nav-link {{ active('reports') }}">Reports</a></li>
         <a href="/overview" class="nav-item nav-link {{ active('overview') }}">Overview</a>
 
-        <a href="{{ route('logout') }}" onclick="event.preventDefault();
-                            document.getElementById('logout-form').submit();"> {{ __('Logout') }} </a>
-        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-          @csrf
-        </form>
-        @else
-        <a href="{{ route('login') }}">Login</a>
-        @if (Route::has('register'))
-        <a href="{{ route('register') }}">Register</a>
-        @endif
       </div>
 
-    </div>
-    @endauth
-    @endif
-  </nav>
-<<<<<<< HEAD
+      @auth
+      <div class="collapse navbar-collapse" id="navbarCollapse">
 
+        <div class="navbar-nav ml-auto">
+          <a href="/home" class="nav-item nav-link {{ active('home') }}">Home</a>
+          <a href="/report" class="nav-item nav-link {{ active('report') }}">Reports</a>
+          <a href="/overview" class="nav-item nav-link {{ active('overview') }}">Overview</a>
+
+          <a href="{{ route('logout') }}" onclick="event.preventDefault();
+                            document.getElementById('logout-form').submit();"> {{ __('Logout') }} </a>
+          <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+            @csrf
+          </form>
+          @else
+          <a href="{{ route('login') }}">Login</a>
+          @if (Route::has('register'))
+          <a href="{{ route('register') }}">Register</a>
+          @endif
+        </div>
+
+      </div>
+      @endauth
+      @endif
+  </nav>
   <div class="container">
-  <p id="output"></p>
+    <p id="output"></p>
 
     @yield('content')
 
   </div>
-=======
   @yield('content')
->>>>>>> 457618502ff46ba920c449d4e78898a4e5ff1c64
 
 </body>
 
